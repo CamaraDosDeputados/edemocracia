0e210ff64d8a5b8664d0e8fd49f59772579b0544
Daniel Dourado Fernandes <daniel.dourado@gmail.com>
2021-04-12 15:32:26 -0300

    wip: updated deputado-detail page

--- src/apps/pjb/views.py
+++ src/apps/pjb/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from apps.pjb.models import DeputadoPjb, ProjetoPjb, ComissaoPjb
+from apps.pjb.models import DeputadoPjb, ProjetoPjb, ComissaoPjb, PartidoPjb, MesaDiretoraPjb
 from django.shortcuts import get_object_or_404
 from apps.wikilegis.data import get_wikilegis_index_data_no_randomness
 
@@ -17,11 +17,59 @@ def deputado_detail_view(request, id):
     except ProjetoPjb.DoesNotExist:
         projeto = None
 
-    comissoes = ComissaoPjb.objects.filter(integrantes__id__in=[deputado.id]).order_by('nome')
+    def get_partido_obj(deputado):
+        obj = None
+        partidos = PartidoPjb.objects.filter(integrantes__id__in=[deputado.id])
+        partido = partidos[0] if len(partidos) > 0 else None
+        atribuicao = "Integrante" if partido else None
+        if atribuicao:
+            atribuicao = "Líder" if partido.lider == deputado else atribuicao
+            atribuicao = "Primeiro Vice-líder" if partido.primeiro_vice_lider == deputado else atribuicao
+            atribuicao = "Segundo Vice-líder" if partido.segundo_vice_lider == deputado else atribuicao
+            atribuicao = "Terceiro Vice-líder" if partido.terceiro_vice_lider == deputado else atribuicao
+            atribuicao = "Quarto Vice-líder" if partido.quarto_vice_lider == deputado else atribuicao
+            atribuicao = f"{atribuicao} do {partido.nome}"
+            obj = {"partido": partido, "atribuicao": atribuicao}
+        return obj
 
+    def get_comissoes_obj(deputado):
+        obj = []
+        comissoes = ComissaoPjb.objects.filter(integrantes__id__in=[deputado.id]).order_by('nome')
+        for comissao in comissoes:
+            atribuicao = "Integrante"
+            atribuicao = "Presidente" if comissao.presidente == deputado else atribuicao
+            atribuicao = "Vice presidente" if comissao.vice_presidente == deputado else atribuicao
+            atribuicao = f"{atribuicao} da {comissao.nome}"
+            obj.append({"comissao": comissao, "atribuicao": atribuicao})
+        return obj
+
+    def get_mesa_diretora_obj(deputado):
+        obj = []
+        mesas = MesaDiretoraPjb.objects.all()
+        for mesa in mesas:
+            atribuicao = None
+            if mesa.presidente == deputado:
+                atribuicao = "Presidente"
+            if mesa.vice_presidente == deputado:
+                atribuicao = "Vice presidente"
+            if mesa.primeiro_secretario == deputado:
+                atribuicao = "Primeiro secretário(a)"
+            if mesa.segundo_secretario == deputado:
+                atribuicao = "Segundo secretário(a)"
+            if atribuicao:
+                atribuicao = f"{atribuicao} da Mesa Diretora"
+                obj.append({"mesa": mesa, "atribuicao": atribuicao})
+        return obj
+
+    comissoes = get_comissoes_obj(deputado)
+    partido_obj = get_partido_obj(deputado)
+    mesa = get_mesa_diretora_obj(deputado)
     return render(request, 'deputados-detail.html', {"deputado": deputado,
                                                      "projeto": projeto,
-                                                     "comissoes": comissoes})
+                                                     "comissoes": comissoes,
+                                                     "partido": partido_obj,
+                                                     "mesas": mesa,
+                                                    })
 
 
 def projeto_list_view(request):
